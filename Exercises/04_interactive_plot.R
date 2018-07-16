3330de664bfceeef3ca31893b2a366a8d774a541
Unknown <flo12392@gmail.com>
2018-07-16 11:19:46 +0200

    modified exercise 4

--- Exercises/04_interactive_plot.R
+++ Exercises/04_interactive_plot.R
@@ -1,9 +1,12 @@
-# Willem wants a car with more chairs, so that whenever he gets more children, they will easily 
-# fit in his car. However, he only wants a car that outperforms his current car.
-# Create an app that allows Willem to select the 'mpg','cyl' or 'disp' or 'hp' column for 
-# both the x-axis and the y axis. Then create a scatter plot of those two columns.
-# Willem also wants to see the dataframe with only the selected columns next to it.
-# Therefore, we will use a reactive to store the dataframe with the subsetted columns.
+# No cool description from this exercise I'm afraid; this one is boring. But if you need a break; here's a link to
+# a joke to lighten the mood: 
+# https://www.reddit.com/r/Jokes/comments/8vx7ym/a_lost_dog_strays_into_a_jungle_a_lion_sees_this/
+
+# Anyway, the goal here is to create an app that allows the user to select the 'mpg','cyl' or 'disp' or 'hp' column 
+# for both the x-axis and the y axis. Then create a scatter plot of those two columns. The user also wants to see the 
+# dataframe with only the selected columns next to it. That last part sounds pretty useless. Why would you want to see 
+# that?  Nobody knows. However, asking questions is not in the job description of a Data Scientist. So we will just go 
+# ahead and execute this plan. We will use a reactive to store the dataframe with the subsetted columns.
 
 library(shiny)
 
